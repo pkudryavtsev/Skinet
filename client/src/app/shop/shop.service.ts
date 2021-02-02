bbfd4ae6bc0425dbc1fc04549beadd6ee8540660
Pavlo <p.kudryavtsev13@gmail.com>
2021-02-02 18:50:45 +0100

    Added routing

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import { IProductType } from '../shared/models/productType';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +43,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.baseUrl}products/${id}`);
   }
 
   getProductBrands(): Observable<IProductBrand[]> {
