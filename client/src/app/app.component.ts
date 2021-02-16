cdf7d76b2e0f9f42220d77af8436ab6251c5c87f
Pavlo <p.kudryavtsev13@gmail.com>
2021-02-16 14:04:22 +0100

    Added Identity to client, added client-side validation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,17 +10,31 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title: 'Skinet';
 
-  constructor(private basketService: BasketService) {
+  constructor(private basketService: BasketService, private accountService: AccountService) {
   }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
         console.log('initialized basket');
       }, error => {
         console.log(error);
-      })
+      });
     }
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('loaded user');
+    }, error => {
+      console.log('error');
+    });
   }
 }
